276731b9e3766ade42d52385b3bd9885e4613ffc
Andrew Sharkey <andrewsharkey@Andrews-MBP-2.fritz.box>
2025-09-27 22:41:44 +0100

    Add GigRepository and interface

--- ApiLambda/src/ApiLambda/Repositories/GigRepository.cs
+++ ApiLambda/src/ApiLambda/Repositories/GigRepository.cs
@@ -1,6 +1,33 @@
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.Core;
+using ApiLambda.Api;
+
 namespace ApiLambda.Repositories;
 
-public class GigRepository
+public class GigRepository(IDynamoDBContext dynamoDbContext, ILambdaLogger logger) : IGigRepository
 {
-    
+    public Task<Gig?> GetByIdAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Gig>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Gig> CreateAsync(Gig gig)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Gig?> UpdateAsync(string id, Gig gig)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- ApiLambda/src/ApiLambda/Repositories/IGigRepository.cs
+++ ApiLambda/src/ApiLambda/Repositories/IGigRepository.cs
@@ -1,6 +1,12 @@
+using ApiLambda.Api;
+
 namespace ApiLambda.Repositories;
 
 public interface IGigRepository
 {
-    
+    Task<Gig?> GetByIdAsync(string id);
+    Task<IEnumerable<Gig>> GetAllAsync();
+    Task<Gig> CreateAsync(Gig gig);
+    Task<Gig?> UpdateAsync(string id, Gig gig);
+    Task<bool> DeleteAsync(string id);
 }
