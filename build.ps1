0b77c8e83ea116c229c959121f0e1960af116402
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.local>
2025-10-15 22:44:18 +0100

    Improve build.ps1 script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,10 @@
+$ErrorActionPreference = "Stop"
+
 dotnet build -c Release
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 zip -j ../Infrastructure/Lambda/GigDiary ./ApiLambda/src/ApiLambda/bin/Release/net8.0/*.*
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 Set-Location ../Infrastructure
 terraform apply -auto-approve
