07b4c3d75e30232451155ce1df3a6df75176cfe7
Andrew Sharkey <andrewsharkey@Andrews-MacBook-Pro-2.local>
2025-11-09 23:54:24 +0000

    Improve GigStats model

--- ApiLambda/src/ApiLambda/Models/GigStats.cs
+++ ApiLambda/src/ApiLambda/Models/GigStats.cs
@@ -1,6 +1,14 @@
+using Amazon.DynamoDBv2.Model;
+
 namespace ApiLambda.Models;
 
-public class GigStats
+public record GigStats
 {
-    
+    public required int GigCount { get; init; }
+    public required decimal EarningsThisYear { get; init; }
+    public required List<KeyValuePair<int, decimal>> EarningsByMonth { get; init; }
+    public required decimal AverageMonthly { get; init; }
+    public required List<KeyValuePair<int, int>> MonthlyGigTally { get; init; }
+    public required int PayerCount { get; init; }
+    public required List<KeyValuePair<string, decimal>> EarningsByPayer { get; init; }
 }
